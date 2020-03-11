6df02cad7260341b71d30c4ff97ecf3f19591193
Bhadoria, Ashutosh <Ashutosh.Bhadoria@dieboldnixdorf>
2020-03-11 14:33:56 +0530

    Add more methods to demonstrate.

--- data-structures-arrays/arrays.js
+++ data-structures-arrays/arrays.js
@@ -1,26 +1,55 @@
-const strings = ['a', 'b', 'c', 'd'];
-// 4*4 = 16 bytes of storage used.
-// JavaScript arrays are by dynamic arrays.
+// Demonstrate common array methods.
 
-console.log(strings[2]);
+const array = [1, 2, 3, 4, 5];
 
-// Insert at the end - push.
-strings.push('e'); // O(1)
-console.log(strings);
-// Push/Append can be O(n) because these are dynamic arrays.
+// Add an element to the end of the array.
+// Returns the length of the array.
+console.log(array.push(6));
+console.log(array);
 
-// Delete from end - pop.
-strings.pop(); // O(1)
-console.log(strings);
+// Remove an element from the end of the array.
+// Returns the removed element.
+console.log(array.pop());
+console.log(array);
 
-// Insert at the beginning - unshift.
-strings.unshift('x'); // O(n)
-console.log(strings);
+// Add an element to the start of the array.
+// Returns the length of the array.
+console.log(array.unshift(10));
+console.log(array);
 
-// Insert in the middle
-strings.splice(2, 0, 'alien'); // O(n)
-console.log(strings);
+// Remove an element from the start of the array.
+// Returns the removed element.
+console.log(array.shift());
+console.log(array);
 
-// Delete from middle
-strings.splice(2, 1); // O(n)
-console.log(strings);
+// Remove an item from any index.
+// splice() takes three arguments -> index from which to start deletion, number of elements to be deleted,
+//                          elements to be added after deletion.
+// Returns an array of deleted elements.
+console.log(array.splice(2, 2));
+console.log(array);
+
+// Insert an item at any index.
+// splice() takes three arguments -> index from which to start deletion, number of elements to be deleted,
+//                          elements to be added after deletion.
+// Returns an array of deleted elements.
+console.log(array.splice(2, 0, 69, 99));
+console.log(array);
+
+// Reverse an array.
+// Returns the reversed array.
+console.log(array.reverse());
+console.log(array);
+
+// Get a portion of an array.
+// slice() -> Takes the index for the portion of the array.
+// Returns a shallow copy of a portion of an array.
+// DOES NOT MODIFY THE ARRAY.
+console.log(array.slice(2,4));
+console.log(array);
+
+// Merge two arrays.
+// concat() does not change the existing arrays, but instead returns a new array.
+// DOES NOT MODIFY THE ARRAY.
+console.log(array.concat(['a', 'b', 'c']));
+console.log(array)
